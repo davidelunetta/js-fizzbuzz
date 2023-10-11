06c2f85c8e86079db5d2ea100726759fddd071f2
davidelunetta <davidelunetta.27@gmail.com>
2023-10-11 20:51:29 +0200

    modified script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-for (let numero = 1; numero <= 100; numero++) {
-    if (numero % 3 === 0 && numero % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (numero % 3 === 0) {
-      console.log("Fizz");
-    } else if (numero % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(numero);
-    }
+function appendToContainer(content) {
+  const container = document.getElementById("container");
+  const element = document.createElement("p");
+  element.textContent = content;
+  container.appendChild(element);
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+      appendToContainer("FizzBuzz");
+  } else if (i % 3 === 0) {
+      appendToContainer("Fizz");
+  } else if (i % 5 === 0) {
+      appendToContainer("Buzz");
+  } else {
+      appendToContainer(i);
   }
+}
